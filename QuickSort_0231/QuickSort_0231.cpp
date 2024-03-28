a204cc183c28ed3fb299877e5a45777514271bdf
Asyurazhr <Asyuradjola2004@gmail.com>
2024-03-28 17:01:42 +0700

    menambahkan void display

--- QuickSort_0231/QuickSort_0231.cpp
+++ QuickSort_0231/QuickSort_0231.cpp
@@ -49,10 +49,10 @@ void q_short(int low, int high)
 	}
 
 	pivot = arr[low];
-	i = low + 1; 
+	i = low + 1;
 	j = high;
 
-	while (i <= j) 
+	while (i <= j)
 	{
 		while ((arr[i] <= pivot) && (i <= high))
 		{
@@ -66,17 +66,34 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
-		if (i < j) 
+		if (i < j)
 		{
 			swap(i, j);
 		}
 
-}
+	}
 	if (low < j) {
 		swap(low, j);
 	}
 	q_short(low, j - 1);
 	q_short(j + 1, high);
+
+}
+
+void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "-----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements:" << mov_count << endl;
+}
+
 
 int main()
 {
